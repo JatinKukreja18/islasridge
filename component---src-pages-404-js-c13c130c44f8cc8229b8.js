fb6bc09956e6958cb33e0fa39e31ae234224dc3c
jatin kukreja <jatinkukreja18@yahoo.in>
2021-05-18 04:49:58 +0530

    Updates

---
+++ component---src-pages-404-js-c13c130c44f8cc8229b8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("7oih"),r=t("EYWl");n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"Not found"}),l.a.createElement("div",{className:"container"},l.a.createElement("h1",null,"Oh no!"),l.a.createElement("h3",null,"We can't seem to find the page you're looking for."),l.a.createElement("br",null)))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c13c130c44f8cc8229b8.js.map
