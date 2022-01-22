3dd967daf95e6195f2b3b1d3f1788365af9ad7fe
KSrahul <rahul.alam@eazydiner.com>
2022-01-22 19:36:52 +0530

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => (
     </p> */}
     <div className="flex">      
       <div className="margin-b-0 flex padding-r-5">
-        <Link to="/tnc">Terms & Conditions</Link>
+        <Link className="tnc-footer" activeClassName="active-menu" to="/tnc">Terms & Conditions</Link> 
       </div>
       <div>&copy; 2021</div> &nbsp;
       <a className="padding-left" href="mailto:islasridge@gmail.com">islasridge@gmail.com</a>
