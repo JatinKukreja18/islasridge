7c03b5f24ef2323b347f652fd6c279dfe27ffe8c
KSrahul <rahul.alam@eazydiner.com>
2022-02-01 21:14:10 +0530

    fixes

--- src/pages/enquire.jsx
+++ src/pages/enquire.jsx
@@ -140,6 +140,9 @@ const Enquire = () => {
                     description="Learn more about us who we are and what we do."
                 />
                 <div className="container">
+                    {/* <svg width="12" height="9" viewBox="0 0 15 9" xmlns="http://www.w3.org/2000/svg"> 
+                        <path d="m13.5 0-6 5.953L1.5 0 0 1.5 7.5 9 15 1.5z" fill="black" fillRule="evenodd"></path>
+                    </svg> */}
                     <div className="m-heading text-center">Enquire</div>
                     <div className="flex contact-main">
                         <div className="map">
@@ -186,9 +189,9 @@ const Enquire = () => {
                                             <div className="flex input-field relative flex-between pointer">
                                                 <input onChange={checkInput} className="absolute hidden-date" type="date" min={todaysDate} name="from"/>
                                                 <div className="select-box-text">{enquireForm.from !== "" ? enquireForm.from : "From"}</div>
-                                                <div className="down-aero">
-                                                    <svg width="12" height="9" viewBox="0 0 15 9" xmlns="http://www.w3.org/2000/svg"> 
-                                                        <path d="m13.5 0-6 5.953L1.5 0 0 1.5 7.5 9 15 1.5z" fill="black" fillRule="evenodd"></path>
+                                                <div className="down-aero flex align-v-center">
+                                                    <svg width="17" height="17" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                        <path fill="#442220" d="M3,111.67,247.67,418.4c4,5.06,12.6,5.06,16.65,0L509,111.64a15.21,15.21,0,0,0,2.2-13.9,14.2,14.2,0,0,0-1-2.32,10.66,10.66,0,0,0-9.39-5.62H11.14a10.64,10.64,0,0,0-9.38,5.62,13.93,13.93,0,0,0-1,2.27A15.26,15.26,0,0,0,3,111.67Z"/>
                                                     </svg>
                                                 </div>
                                             </div>
@@ -201,9 +204,9 @@ const Enquire = () => {
                                             <div className="flex input-field flex-between relative pointer">
                                                 <input onChange={checkInput} className="absolute hidden-date" type="date" min={todaysDate} name="to"/>
                                                 <div className="select-box-text">{enquireForm.to !== "" ? enquireForm.to : "To"}</div>
-                                                <div className="down-aero">
-                                                    <svg width="12" height="9" viewBox="0 0 15 9" xmlns="http://www.w3.org/2000/svg"> 
-                                                        <path d="m13.5 0-6 5.953L1.5 0 0 1.5 7.5 9 15 1.5z" fill="black" fillRule="evenodd"></path>
+                                                <div className="down-aero flex align-v-center">
+                                                    <svg width="17" height="17" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                        <path fill="#442220" d="M3,111.67,247.67,418.4c4,5.06,12.6,5.06,16.65,0L509,111.64a15.21,15.21,0,0,0,2.2-13.9,14.2,14.2,0,0,0-1-2.32,10.66,10.66,0,0,0-9.39-5.62H11.14a10.64,10.64,0,0,0-9.38,5.62,13.93,13.93,0,0,0-1,2.27A15.26,15.26,0,0,0,3,111.67Z"/>
                                                     </svg>
                                                 </div>
                                             </div>
@@ -219,9 +222,9 @@ const Enquire = () => {
                                                     <div key={index} className="flex-1 relative select-container">
                                                         <div onClick={() => selectHeading(index)} className="flex input-field flex-between pointer align-v-center" aria-hidden="true">
                                                             <div className="select-box-text">{enquireForm[data.name] !== "" ? enquireForm[data.name] : data.title}</div>
-                                                            <div className="down-aero">
-                                                                <svg width="12" height="9" viewBox="0 0 15 9" xmlns="http://www.w3.org/2000/svg"> 
-                                                                    <path d="m13.5 0-6 5.953L1.5 0 0 1.5 7.5 9 15 1.5z" fill="black" fillRule="evenodd"></path>
+                                                            <div className="down-aero flex align-v-center">
+                                                                <svg width="17" height="17" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                                                    <path fill="#442220" d="M3,111.67,247.67,418.4c4,5.06,12.6,5.06,16.65,0L509,111.64a15.21,15.21,0,0,0,2.2-13.9,14.2,14.2,0,0,0-1-2.32,10.66,10.66,0,0,0-9.39-5.62H11.14a10.64,10.64,0,0,0-9.38,5.62,13.93,13.93,0,0,0-1,2.27A15.26,15.26,0,0,0,3,111.67Z"/>
                                                                 </svg>
                                                             </div>
                                                         </div>
