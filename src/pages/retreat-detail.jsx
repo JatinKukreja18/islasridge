4f772457db1379f31294d72526c0debcc6fe9303
KSrahul <rahul.alam@eazydiner.com>
2022-02-08 08:48:36 +0530

    fixes

--- src/pages/retreat-detail.jsx
+++ src/pages/retreat-detail.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import Layout from '../components/Layout';
 import kiteImg from '../images/kitePink.png'
 import { navigate } from 'gatsby';
-import {retreatsData} from '../components/RetreatData'
+import {retreatsData} from '../components/RetreatData';
+import SEO from '../components/SEO'
 
 const RetreatDetail = (props) => {
     // const pageData = props.location.state && props.location.state.pageData;
@@ -25,35 +26,41 @@ const RetreatDetail = (props) => {
             </style>
             <Layout>
                 {
-                    filterPageData ? 
-                        <div className="container space-for-header">
-                            <div className="breadcrumb padding-b-5">
-                                <Link to="/retreats">Retreats</Link>
-                                <span className="seprator"> / </span>
-                                <span className="added">{filterPageData.breadcrumbTitle}</span>
-                            </div>
-                            {
-                                filterPageData.listRetreats.map((data, index) =>{
-                                    return(
-                                        <div className="retreat-main" key={index}>
-                                            <div>
-                                                <img className="full-width left-img img-cover" src={data.image} height="540" alt="" />
-                                            </div>
-                                            <div className="flex align-v-center retreat-heading-main">
-                                                <div className="">
-                                                    <div className="flex align-v-center retreat-heading">
-                                                        <img className="" src={kiteImg} alt="" />
-                                                        <h5 className="retrea-title padding-left-20">{data.heading}</h5>
+                    filterPageData ?
+                        <>
+                            <SEO
+                                title= {`${filterPageData.breadcrumbTitle}`}
+                                description="Learn more about us who we are and what we do."
+                            />
+                            <div className="container space-for-header">
+                                <div className="breadcrumb padding-b-5">
+                                    <Link to="/retreats">Retreats</Link>
+                                    <span className="seprator"> / </span>
+                                    <span className="added">{filterPageData.breadcrumbTitle}</span>
+                                </div>
+                                {
+                                    filterPageData.listRetreats.map((data, index) =>{
+                                        return(
+                                            <div className="retreat-main" key={index}>
+                                                <div>
+                                                    <img className="full-width left-img img-cover" src={data.image} height="540" alt="" />
+                                                </div>
+                                                <div className="flex align-v-center retreat-heading-main">
+                                                    <div className="">
+                                                        <div className="flex align-v-center retreat-heading">
+                                                            <img className="" src={kiteImg} alt="" />
+                                                            <h5 className="retrea-title padding-left-20">{data.heading}</h5>
+                                                        </div>
+                                                        <p className="margin-b-0 retrea-desc">{data.description}</p>
+                                                        <Link className="more-retreat" to="/enquire">ENQUIRE</Link>
                                                     </div>
-                                                    <p className="margin-b-0 retrea-desc">{data.description}</p>
-                                                    <Link className="more-retreat" to="/enquire">ENQUIRE</Link>
                                                 </div>
                                             </div>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </>
                     : typeof window !== `undefined` && navigate('/404/')
                 }
             </Layout>
--- src/pages/room-details.jsx
+++ src/pages/room-details.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../components/Layout'
 import { navigate } from 'gatsby';
 import { roomsData } from '../components/RoomsData';
+import SEO from '../components/SEO';
 
 const rooms = (props) => {
     // const pageData = props.location.state && props.location.state.pageData;
@@ -26,31 +27,38 @@ const rooms = (props) => {
             <Layout>
                 {
                     filterPageData ?
-                        <div className="container space-for-header">
-                            <div className="full-banner">
-                                <div className="breadcrumb padding-b-5">
-                                    <Link to="/rooms">Rooms</Link>
-                                    <span className="seprator"> / </span>
-                                    <span>{filterPageData.breadcrumbTitle}</span>
+                        <>
+                            <SEO
+                                title= {`${filterPageData.breadcrumbTitle}`}
+                                description={`${filterPageData.roomDescription}`}
+                            />
+
+                            <div className="container space-for-header">
+                                <div className="full-banner">
+                                    <div className="breadcrumb padding-b-5">
+                                        <Link to="/rooms">Rooms</Link>
+                                        <span className="seprator"> / </span>
+                                        <span>{filterPageData.breadcrumbTitle}</span>
+                                    </div>
+                                    <img className="full-width img-cover" src={filterPageData.featuredImage} height="455" alt="" /> 
+                                    <h5 className="room-name">{filterPageData.roomHeading}</h5>
+                                    <p>{filterPageData.roomPara}</p>
+                                    <div className="room-img-main">
+                                        {
+                                            filterPageData.roomsGallery.map((data, index) =>{
+                                                return(
+                                                    <div className="room-img" key={index}>
+                                                        <img className="full-width img-cover" src={data} height="282" alt="" />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <div className="room-bottom-desc">{filterPageData.roomDescription}</div>
+                                    <Link className="more-retreat" to="/enquire">ENQUIRE </Link>
                                 </div>
-                                <img className="full-width img-cover" src={filterPageData.featuredImage} height="455" alt="" /> 
-                                <h5 className="room-name">{filterPageData.roomHeading}</h5>
-                                <p>{filterPageData.roomPara}</p>
-                                <div className="room-img-main">
-                                    {
-                                        filterPageData.roomsGallery.map((data, index) =>{
-                                            return(
-                                                <div className="room-img" key={index}>
-                                                    <img className="full-width img-cover" src={data} height="282" alt="" />
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <div className="room-bottom-desc">{filterPageData.roomDescription}</div>
-                                <Link className="more-retreat" to="/enquire">ENQUIRE </Link>
                             </div>
-                        </div>
+                        </>
                     : typeof window !== `undefined` && navigate('/404/')
                 }
             </Layout>
