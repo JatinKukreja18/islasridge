b979ba2571532c0c8808c70742eb77bfad9f8f30
KSrahul <rahul.alam@eazydiner.com>
2022-02-08 21:41:21 +0530

    fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = ({ isHomepage }) => {
             headerMenus.map((data, index) =>{
               return(
                 <li key={index}>
-                  <Link activeClassName="active-menu" exact="true" to={data.link} onClick={openHamMenus}>{data.title}</Link>
+                  <Link activeClassName="active-menu" exact={true} to={data.link} onClick={openHamMenus}>{data.title}</Link>
                 </li>
               )
             })
